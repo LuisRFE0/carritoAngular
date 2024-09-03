86e034827dec6f224e364ae27e441a2f2abccfb0
Luis Flores <luis.r.flores.e@hotmail.com>
2024-09-03 10:22:18 -0600

    inicio de carro compra

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: "Teclado mecanico asus",
+        description: "Teclado mecanico red",
+        price: 1000
+    },
+    {
+        id: 2,
+        name: "Monitor asus",
+        description: "Monitor 27 pulgadas",
+        price: 2000
+    },
+    {
+        id: 3,
+        name: "Raton corsair",
+        description: "Corsair MG300",
+        price: 500
+    },
+    {
+        id: 4,
+        name: "Laptop Gamer",
+        description: "Laptop Omen",
+        price: 20000
+    },
+    {
+        id: 5,
+        name: "Pc Gamer",
+        description: "Pc para videojuegos",
+        price: 15000
+    },
+    {
+        id: 6,
+        name: "Gabinete Cooler Master",
+        description: "Gabinete RGB",
+        price: 800
+    }
+];
